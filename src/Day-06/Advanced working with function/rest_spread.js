2cab836960398f3215a074cb9d96043309421627
knilesh2212 <knilesh2212@gmail.com>
2025-12-01 21:41:59 +0530

    feat: upload Day-06

---
+++ src/Day-06/Advanced working with function/rest_spread.js
@@ -0,0 +1,24 @@
+// rest and spread operators
+
+function sum(a, b) {
+  return a + b;
+}
+
+alert(sum(1, 2, 3, 4, 5));
+
+// rest perameters
+function sumAll(...args) {
+  let total = 0;
+  for (let arg of args) total += arg;
+  return total;
+}
+console.log(sumAll(1, 2, 3, 4, 5)); // 15
+console.log(sumAll(10, 20, 30)); // 60
+
+
+// spread operator
+let arr = [1, 2, 3];
+console.log(...arr); // 1 2 3
+let arr2 = [4, 5, 6];
+let combined = [...arr, ...arr2];
+console.log(combined); // [1, 2, 3, 4, 5, 6]
